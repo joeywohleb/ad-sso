f1e54b6e32b567904759106130a0fe51307d8124
unknown <hjw18543@9KNKNS1.mfc.corp.mckee.com>
2013-10-16 13:02:43 -0400

    Initiated plugin headers for Wordpress.

---
+++ ad-sso.php
@@ -0,0 +1,10 @@
+<?php   
+    /* 
+    Plugin Name: Active Directory Single Sign On
+    Plugin URI: https://github.com/joeywohleb/ad-sso
+    Description: Wordpress plugin for implenting Single Sign On for Active Directory.
+    Author: Joey Wohleb
+    Version: 0.1
+    Author URI: http://joeywohleb.com/
+    */  
+?>
